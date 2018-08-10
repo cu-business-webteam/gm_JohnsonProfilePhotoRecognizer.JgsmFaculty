a7eb5b516b65d960acb4d088f10278d2549611fb
uniblab <tjb284@cornell.edu>
2018-08-10 14:54:02 -0400

    x

--- src/Configuration/FileRecognizerConfigurationElement.cs
+++ src/Configuration/FileRecognizerConfigurationElement.cs
@@ -12,6 +12,8 @@ namespace Johnson.ProfilePhotoRecognizer.JgsmFaculty.Configuration {
 
 
 		#region properties
+		[System.IO.IODescription( "Specifies the Oits.Configuration.DbMap group." )]
+		[System.Configuration.ConfigurationProperty( "groupName", IsRequired = true, IsKey = false )]
 		public System.String GroupName {
 			get {
 				return ( this[ "groupName" ] as System.String );
@@ -20,6 +22,8 @@ namespace Johnson.ProfilePhotoRecognizer.JgsmFaculty.Configuration {
 				this[ "groupName" ] = value;
 			}
 		}
+		[System.IO.IODescription( "Specifies the Oits.Configuration.DbMap command which matches up the file with a web profile." )]
+		[System.Configuration.ConfigurationProperty( "commandName", IsRequired = true, IsKey = false )]
 		public System.String CommandName {
 			get {
 				return ( this[ "commandName" ] as System.String );
@@ -28,6 +32,8 @@ namespace Johnson.ProfilePhotoRecognizer.JgsmFaculty.Configuration {
 				this[ "commandName" ] = value;
 			}
 		}
+		[System.IO.IODescription( "Specifies the Oits.Configuration.DbMap result mapping." )]
+		[System.Configuration.ConfigurationProperty( "resultName", IsRequired = true, IsKey = false )]
 		public System.String ResultName {
 			get {
 				return ( this[ "resultName" ] as System.String );
@@ -37,6 +43,8 @@ namespace Johnson.ProfilePhotoRecognizer.JgsmFaculty.Configuration {
 			}
 		}
 
+		[System.IO.IODescription( "The regular expression for isolataing the NetId from the file name." )]
+		[System.Configuration.ConfigurationProperty( "netIdExpression", IsRequired = true, IsKey = false )]
 		public System.String NetIdExpression {
 			get {
 				return ( this[ "netIdExpression" ] as System.String );
@@ -46,6 +54,7 @@ namespace Johnson.ProfilePhotoRecognizer.JgsmFaculty.Configuration {
 			}
 		}
 
+		[System.IO.IODescription( "The directory to where the file should be moved." )]
 		[System.Configuration.ConfigurationProperty( "destination", IsRequired = true, IsKey = false )]
 		public System.String Destination {
 			get {
